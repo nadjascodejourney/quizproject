e320ae5c5bc017a851c2286286b4e150f77269f4
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-05-10 11:39:35 +0200

    updateChosenAnswer

--- src/components/Karusell.jsx
+++ src/components/Karusell.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import quizdata from "../data/quizdata.json";
+import { useState } from "react";
 
 const GetQuestionPartList = ({ answers }) => {
   return (
@@ -12,13 +13,31 @@ const GetQuestionPartList = ({ answers }) => {
 };
 
 const Karusell = () => {
+  const [chosenAnswer, setChosenAnswer] = useState([]);
+
+  const updateChosenAnswer = () => {
+  }
+
   return (
     <div>
       {quizdata.map((question) => (
         <div key={question.questionId}>
           <h3>{question.question}</h3>
-          <GetQuestionPartList answers={question.partone} />
-          <GetQuestionPartList answers={question.parttwo} />
+
+          <ul>
+            {question.partone.map((answer, id) => (
+              <li key={id}>{answer}</li>
+            ))}
+          </ul>
+
+          <ul>
+            {question.parttwo.map((answer, id) => (
+              <li key={id} onSelect={(answer) => ()}>{answer}</li>
+            ))}
+          </ul>
+
+          {/* <GetQuestionPartList answers={question.partone} />
+          <GetQuestionPartList answers={question.parttwo} /> */}
         </div>
       ))}
     </div>
