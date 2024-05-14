304bfae171284e0476b1728b3c7125d8e0a777cd
Philipp Zacharaias <philipp_zacharias@icloud.com>
2024-05-14 15:27:35 +0200

    style&fontAwesome

--- src/components/AnswerMultiple.jsx
+++ src/components/AnswerMultiple.jsx
@@ -2,7 +2,12 @@
  */
 /* import CarouselListItems from "./CarouselListItems"; */
 // styles
-import styles from "../styles/answerMulti.module.css"
+import styles from "../styles/answerMulti.module.css";
+
+//FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 import { useContext } from "react";
 import { QuizContext } from "../utils/StateManagement";
@@ -12,33 +17,53 @@ const AnswerMultiple = () => {
 
   return (
     <div>
-    <section className={styles.answerContainer}>
-      {/* Oberes Karusell */}
-      {/*  <ArrowButton onClick={() => updateAnswersOrder(0, "-")} /> */}
-      <button onClick={() => updateAnswersOrder(0, "-")} className={styles.answerMultiButton}>{"<"}</button>
-      {/*  <CarouselListItems answers={userAnswer[0]} /> */}
-      <ul>
-        {userAnswer[0]?.length <= 1 && <li></li>}
-        {userAnswer[0]?.map((answer, id) => (
-          <li key={id}>{answer}</li>
-        ))}
-      </ul>
-      <button onClick={() => updateAnswersOrder(0, "+")} className={styles.answerMultiButton}>{">"}</button>
-    </section>
+      <section className={styles.answerContainer}>
+        {/* Oberes Karusell */}
+        {/*  <ArrowButton onClick={() => updateAnswersOrder(0, "-")} /> */}
+        <button
+          onClick={() => updateAnswersOrder(0, "-")}
+          className={styles.answerMultiButton}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        {/*  <CarouselListItems answers={userAnswer[0]} /> */}
+        <ul>
+          {userAnswer[0]?.length <= 1 && <li></li>}
+          {userAnswer[0]?.map((answer, id) => (
+            <li key={id}>{answer}</li>
+          ))}
+        </ul>
+        <button
+          onClick={() => updateAnswersOrder(0, "+")}
+          className={styles.answerMultiButton}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </section>
       {/*  <ArrowButton onClick={() => updateAnswersOrder(0, "+")} /> */}
-    <section className={styles.answerContainer}>
-      {/* Unteres Karusell */}
-      {/*  <ArrowButton onClick={() => updateAnswersOrder(1, "-")} /> */}
-      <button onClick={() => updateAnswersOrder(1, "-")} className={styles.answerMultiButton}>{"<"}</button>
-      {/*   <CarouselListItems answers={userAnswer[1]} /> */}
-      <ul>
-        {userAnswer[1]?.length <= 1 && <li></li>}
-        {userAnswer[1]?.map((answer, id) => (
-          <li key={id}>{answer}</li>
-        ))}
-      </ul>
-      {/* <ArrowButton onClick={() => updateAnswersOrder(1, "+")} /> */}
-      <button onClick={() => updateAnswersOrder(1, "+")} className={styles.answerMultiButton}>{">"}</button>
+      <section className={styles.answerContainer}>
+        {/* Unteres Karusell */}
+        {/*  <ArrowButton onClick={() => updateAnswersOrder(1, "-")} /> */}
+        <button
+          onClick={() => updateAnswersOrder(1, "-")}
+          className={styles.answerMultiButton}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        {/*   <CarouselListItems answers={userAnswer[1]} /> */}
+        <ul>
+          {userAnswer[1]?.length <= 1 && <li></li>}
+          {userAnswer[1]?.map((answer, id) => (
+            <li key={id}>{answer}</li>
+          ))}
+        </ul>
+        {/* <ArrowButton onClick={() => updateAnswersOrder(1, "+")} /> */}
+        <button
+          onClick={() => updateAnswersOrder(1, "+")}
+          className={styles.answerMultiButton}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
       </section>
     </div>
   );
